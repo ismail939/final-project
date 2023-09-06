9e9f5e927fa41cc6bb332c1c2dc8dba4611a2b87
ismail939 <ismailsalama526@gmail.com>
2023-09-06 19:18:22 +0300

    register and login ready

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<link>
+<meta charset="UTF-8">
+<meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Login</title>
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+
+<body>
+    <form action="{{route('userHome')}}" method="GET" id="loginForm">
+        <label>Email</label><br>
+        <input type="email" name="email" id="email" placeholder="enter your email"><br>
+        <label>Password</label><br>
+        <input type="password" name="password" id="password" placeholder="enter your password"><br>
+        <input type="submit" value="login">
+
+
+    </form>
+    <form action="{{ route('register') }}" method="GET"><button>register</button></form>
+</body>
+
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<link>
+<meta charset="UTF-8">
+<meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Login</title>
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+
+<body>
+    <form action="{{ route('userHome') }}" method="GET" id="loginForm" enctype="multipart/form-data">
+        <label>Name</label><br>
+        <input type="text" name="name" id="name" placeholder="enter your full name"><br>
+        <label>Email</label><br>
+        <input type="email" name="email" id="email" placeholder="enter your email"><br>
+        <label>Password</label><br>
+        <input type="password" name="password" id="password" placeholder="enter your password"><br>
+        <label>Address</label><br>
+        <input type="text" name="address" id="address" placeholder="enter your address"><br>
+        <label>Profile picture</label><br>
+        <input type="file" name="img" id="img"><br>
+        <input type="submit" value="submit">
+
+    </form>
+
+</body>
+
+</html>
