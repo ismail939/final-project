ac2b4a2f0205b7183075f6a1dbce4bf4fca27747
ismail939 <ismailsalama526@gmail.com>
2023-09-08 21:57:41 +0300

    all tables created i think

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Order extends Model
 {
     use HasFactory;
-    function category()
-    {
-        return $this->belongsTo(Category::class);
+    function user(){
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/Orderproduct.php
+++ app/Models/Orderproduct.php
@@ -5,11 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Orderproduct extends Model
 {
     use HasFactory;
-    function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
+    
 }
